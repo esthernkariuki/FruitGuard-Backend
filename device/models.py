469c7df2d2f3ce048efc601486b21afb81def9fd
Shabach Muriithi <shabachmuriithi@gmail.com>
2025-09-09 16:00:51 +0300

    renamed trap_id to device_id

--- device/models.py
+++ device/models.py
@@ -9,7 +9,7 @@ class Device(models.Model):
         ('inactive', 'Inactive'),
 ]
 
-    trap_id = models.BigAutoField(primary_key=True)
+    device_id = models.BigAutoField(primary_key=True)
     status= models.CharField(max_length=50, choices= DEVICE_STATUS, default='Active')
     user_id=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_at = models.DateField(auto_now_add=True)
