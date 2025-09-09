f1522acbf5a7da65a3a28a380cd5b8d23edbb35c
Elizabeth Barongo <elizabethmoraab@gmail.com>
2025-09-09 13:33:04 +0300

    made users model

---
+++ users/models.py
@@ -0,0 +1,52 @@
+
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+USER_TYPE_CHOICES = [
+    ('farmer', 'Farmer'),
+    ('agrovet', 'Agrovet'),
+    ('admin', 'Admin'),
+]
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('user_type', 'admin')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    username = None  
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone_number = models.CharField(max_length=50)
+    email = models.EmailField('email address', blank=True, null=True, unique=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    user_type = models.CharField(max_length=50, choices=USER_TYPE_CHOICES, default='farmer')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'user_type']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.user_type})"
