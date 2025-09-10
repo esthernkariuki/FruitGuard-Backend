d9782e1df79d536f6f24e5d81551328b57d9d3ee
Your Name <you@example.com>
2025-09-10 07:02:22 +0300

    made some changes

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,4 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import DataMonitoringViewSet
 
-# router = DefaultRouter()
-# router.register(r"data_monitoring", DataMonitoringViewSet, basename="data_monitoring")
-
-# urlpatterns = [
-#     path("", include(router.urls)),
-# ]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DataMonitoringViewSet
